5f14a85effbdf34f60d466590ff8d49edd64fb2c
fatooo0116@gmail.com <cbsi940424>
2021-03-25 11:23:25 +0800

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+import { DebounceInput } from "react-debounce-input";
+import "./App.scss";
+
+import Panel from "./myapp/Panel";
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 6,
+      postTypes: {},
+      postResults: {},
+      pages: {},
+      results: "",
+      blogname: "",
+      initialRender: false
+    };
+    this.handleBlognameChange = this.handleBlognameChange.bind(this);
+  }
+
+  handleBlognameChange(event) {
+    this.setState({
+      blogname: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>WP React Plugin</h1>
+        <div>
+
+        <Panel />
+
+          <div className="input-container">
+            <label htmlFor="normal-input">
+              <span>Normal Input</span>
+              <input
+                className="n-input"
+                id="normal-input"
+                value={this.state.blogname}
+                onChange={this.handleBlognameChange}
+              />
+            </label>
+
+            <label htmlFor="debounced-input">
+              <span>Debounced Input</span>
+              <DebounceInput
+                className="d-input"
+                id="debounced-input"
+                value={this.state.blogname}
+                minLength={0}
+                debounceTimeout={300}
+                onChange={this.handleBlognameChange}
+              />
+            </label>
+          </div>
+          <div style={{ marginTop: 20 }}>
+            You entered: {this.state.blogname}
+            <span id="wp-spinner" className="spinner" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default hot(module)(App);
